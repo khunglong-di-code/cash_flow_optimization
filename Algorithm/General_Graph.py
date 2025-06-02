e00e10d7a4248f25f612cc7ea41286e68305b9e5
ThoaTrieu <thoatrieu72@gmail.com>
2025-06-02 09:54:48 +0700

    Update General_Graph.py

--- Algorithm/General_Graph.py
+++ Algorithm/General_Graph.py
@@ -2,230 +2,130 @@ from Algorithm.Check_Graph import isTree
 
 def FIND_TRANSACTIONS(vertex_data, edge_data):
     V = {'V': [], 'w': []}
-    i = 0
-    while i < 1000:  
-        if i >= len(vertex_data):
-            break
-        V['V'] += [vertex_data[i]]
-        V['w'] += [0]
-        i += 1
+    for i in range(len(vertex_data)):
+        V['V'].append(vertex_data[i][0])
+        V['w'].append(0)
 
     T = {'V': [], 'E': [], 'adj': {}}
-    i = 0
-    while i < len(vertex_data):
-        T['V'] += [vertex_data[i]]
-        T['adj'][vertex_data[i]] = []
-        i += 1
+    for i in range(len(vertex_data)):
+        u = vertex_data[i][0]
+        T['V'].append(u)
+        T['adj'][u] = []
 
-    i = 0
-    while i < len(edge_data):
-        u = edge_data[i][0]
-        v = edge_data[i][1]
-        w = edge_data[i][2]
-
-        T['E'] += [(u, v)]
-        T['adj'][u] += [v]
-
-        j = 0
-        while j < len(V['V']):
+    for i in range(len(edge_data)):
+        u, v, w = edge_data[i]
+        T['E'].append((u, v))
+        T['adj'][u].append(v)
+        for j in range(len(V['V'])):
             if V['V'][j] == u:
-                V['w'][j] = V['w'][j] - w
+                V['w'][j] -= w
             if V['V'][j] == v:
-                V['w'][j] = V['w'][j] + w
-            j += 1
-        i += 1
+                V['w'][j] += w
 
     transactions = []
     n = len(V['V'])
-
     V_copy = {'V': [], 'w': []}
-    i = 0
-    while i < n:
-        V_copy['V'] += [V['V'][i]]
-        V_copy['w'] += [V['w'][i]]
-        i += 1
+    for i in range(n):
+        V_copy['V'].append(V['V'][i])
+        V_copy['w'].append(V['w'][i])
 
     T_copy = {'V': [], 'E': [], 'adj': {}}
-    i = 0
-    while i < len(T['V']):
-        T_copy['V'] += [T['V'][i]]
-        T_copy['adj'][T['V'][i]] = []
-        i += 1
-    i = 0
-    while i < len(T['E']):
-        T_copy['E'] += [T['E'][i]]
-        i += 1
-    for k in T['adj']:
-        T_copy['adj'][k] = []
-        j = 0
-        while j < len(T['adj'][k]):
-            T_copy['adj'][k] += [T['adj'][k][j]]
-            j += 1
+    for i in range(len(T['V'])):
+        u = T['V'][i]
+        T_copy['V'].append(u)
+        T_copy['adj'][u] = []
+    for i in range(len(T['E'])):
+        T_copy['E'].append(T['E'][i])
+    for u in T['adj']:
+        for v in T['adj'][u]:
+            T_copy['adj'][u].append(v)
 
     def isTree(vertices, adj):
         visited = {}
         for v in vertices:
             visited[v] = False
-
         def dfs(u, parent):
             visited[u] = True
-            i = 0
-            while i < len(adj[u]):
-                w = adj[u][i]
-                if visited[w] == False:
+            for w in adj[u]:
+                if not visited[w]:
                     if not dfs(w, u):
                         return False
                 elif w != parent:
                     return False
-                i += 1
             return True
-
         if not dfs(vertices[0], -1):
             return False
-
         for v in visited:
-            if visited[v] == False:
+            if not visited[v]:
                 return False
         return True
 
     if not isTree(T_copy['V'], T_copy['adj']):
         visited = {}
-        i = 0
-        while i < len(T_copy['V']):
-            visited[T_copy['V'][i]] = False
-            i += 1
+        for v in T_copy['V']:
+            visited[v] = False
         spanning_tree_edges = []
         new_adj = {}
-        i = 0
-        while i < len(T_copy['V']):
-            new_adj[T_copy['V'][i]] = []
-            i += 1
-
+        for v in T_copy['V']:
+            new_adj[v] = []
         def DFS(u):
             visited[u] = True
-            i = 0
-            while i < len(T_copy['adj'][u]):
-                v = T_copy['adj'][u][i]
-                if visited[v] == False:
-                    spanning_tree_edges += [(u, v)]
-                    new_adj[u] += [v]
-                    new_adj[v] += [u]
+            for v in T_copy['adj'][u]:
+                if not visited[v]:
+                    spanning_tree_edges.append((u, v))
+                    new_adj[u].append(v)
+                    new_adj[v].append(u)
                     DFS(v)
-                i += 1
-
         DFS(T_copy['V'][0])
         T_copy['E'] = spanning_tree_edges
         T_copy['adj'] = new_adj
 
     parent = {}
-    i = 0
-    while i < len(T_copy['V']):
-        parent[T_copy['V'][i]] = -1
-        i += 1
-
+    for v in T_copy['V']:
+        parent[v] = -1
     def set_parents(u, p):
         parent[u] = p
-        i = 0
-        while i < len(T_copy['adj'][u]):
-            v = T_copy['adj'][u][i]
+        for v in T_copy['adj'][u]:
             if v != p:
                 set_parents(v, u)
-            i += 1
-
     set_parents(T_copy['V'][0], -1)
 
     temp_adj = {}
-    i = 0
-    while i < len(T_copy['V']):
-        u = T_copy['V'][i]
+    for u in T_copy['V']:
         temp_adj[u] = []
-        j = 0
-        while j < len(T_copy['adj'][u]):
-            temp_adj[u] += [T_copy['adj'][u][j]]
-            j += 1
-        i += 1
+        for v in T_copy['adj'][u]:
+            temp_adj[u].append(v)
 
     remaining_vertices = []
-    i = 0
-    while i < len(T_copy['V']):
-        remaining_vertices += [T_copy['V'][i]]
-        i += 1
+    for v in T_copy['V']:
+        remaining_vertices.append(v)
 
-    while True:
-        count = 0
-        i = 0
-        while i < len(remaining_vertices):
-            count += 1
-            i += 1
-        if count <= 1:
-            break
-
+    while len(remaining_vertices) > 1:
         vc = -1
-        i = 0
-        while i < len(remaining_vertices):
-            node = remaining_vertices[i]
-            deg = 0
-            j = 0
-            while j < len(temp_adj[node]):
-                deg += 1
-                j += 1
-            if deg == 1:
+        for node in remaining_vertices:
+            if len(temp_adj[node]) == 1:
                 vc = node
                 break
-            i += 1
-
         if vc == -1:
             break
-        if len(temp_adj[vc]) > 0:
-            vp = temp_adj[vc][0]
-        else:
-            vp = -1
+        vp = temp_adj[vc][0] if temp_adj[vc] else -1
         if vp == -1:
             break
-
-        i = 0
-        while i < len(V_copy['V']):
+        for i in range(len(V_copy['V'])):
             if V_copy['V'][i] == vc:
                 w_vc = V_copy['w'][i]
-            i += 1
-
         if w_vc > 0:
-            transactions += [(vc, vp, w_vc)]
+            transactions.append((vc, vp, w_vc))
         else:
-            transactions += [(vp, vc, -w_vc)]
-
-        i = 0
-        while i < len(V_copy['V']):
+            transactions.append((vp, vc, -w_vc))
+        for i in range(len(V_copy['V'])):
             if V_copy['V'][i] == vp:
                 idx_vp = i
             if V_copy['V'][i] == vc:
                 idx_vc = i
-            i += 1
-        V_copy['w'][idx_vp] = V_copy['w'][idx_vp] + V_copy['w'][idx_vc]
-
-        new_temp = []
-        i = 0
-        while i < len(temp_adj[vp]):
-            if temp_adj[vp][i] != vc:
-                new_temp += [temp_adj[vp][i]]
-            i += 1
-        temp_adj[vp] = new_temp
-
-        new_temp = []
-        i = 0
-        while i < len(temp_adj[vc]):
-            if temp_adj[vc][i] != vp:
-                new_temp += [temp_adj[vc][i]]
-            i += 1
-        temp_adj[vc] = new_temp
-
-        new_remaining = []
-        i = 0
-        while i < len(remaining_vertices):
-            if remaining_vertices[i] != vc:
-                new_remaining += [remaining_vertices[i]]
-            i += 1
-        remaining_vertices = new_remaining
+        V_copy['w'][idx_vp] += V_copy['w'][idx_vc]
+        temp_adj[vp] = [v for v in temp_adj[vp] if v != vc]
+        temp_adj[vc] = [v for v in temp_adj[vc] if v != vp]
+        remaining_vertices = [v for v in remaining_vertices if v != vc]
 
     return transactions
-
